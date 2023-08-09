c504d07642ed860a504f9c88dcd6fb709c7509b7
Sulaiman1235 <112854352+Sulaiman1235@users.noreply.github.com>
2023-08-09 13:05:08 +0300

    Add files via upload

---
+++ Koodit/memory game/script.js
@@ -0,0 +1,127 @@
+const squaresContainer = document.querySelector("#squares");
+const numberOfSquares = 16;
+let i = 0;
+
+let square1, square2
+let clickCount = 0;
+let score = 0;
+document.querySelector("#score").style.visibilty = "hidden";
+const playAgainBtn = document.querySelector('button')
+playAgainBtn.style.visibility = "hidden";
+playAgainBtn.addEventListener('click', playAgainBtn)
+let colors = [
+
+    "#33ff33",
+    "#33ff33",
+    "#ff944f",
+    "#ff944f",
+    "#80ccff",
+    "#80ccff",
+    "#ffff66",
+    "#ffff66",
+    "#ff4dff",
+    "#ff4dff",
+    "#ff1a1a",
+    "#ff1a1a",
+    "#dddddd",
+    "#dddddd",
+    "#57e108",
+    "#57e108",
+
+]
+
+function selectColor() {
+    const random = Math.floor(Math.random() * colors.length)
+    const selected= colors[random]
+    colors.splice(random, 1)
+    return selected;
+
+}
+
+while (i < numberOfSquares) {
+    const square = document.createElement('li');
+    const color = selectColor();
+   // square.style.background = color;
+    square.setAttribute("data-color",color);
+    squaresContainer.appendChild(square);
+    i++;
+}
+
+const squares = document.querySelectorAll('li')
+ for(const square of squares) {
+    square.addEventListener("click", squareClicked);
+ }
+
+ function squareClicked() {
+    if(square1 == this) return;
+    clickCount++;
+    if(clickCount > 2 )return;
+clickCount === 1 ? (square1 = this) : (square2 = this)
+if(clickCount== 1 ) {
+    square1.style.background = square1.getAttribute("data-color");
+}else{
+    square2.style.background = square2.getAttribute("data-color");
+    checkMatch();
+
+}
+
+function checkMatch() {
+
+    let match =
+    square1.getAttribute("data-color") === square2.getAttribute("data-color")
+    if (!match) {
+        setTimeout(function (){
+            noMatch();
+        }, 500);
+        
+    }else{
+        isMatch();
+    }
+
+
+
+}
+ }
+
+function noMatch() {
+    square1.style.background = "";
+    square2.style.background = "";
+    square1 = ""
+    square2 = ""
+    clickCount = 0;
+    console.log("no match");
+}
+function isMatch() {
+    score++;
+    document.querySelector("#score").innerText = score;
+    document.querySelector("#score").style.visibilty = "visible";
+    square1.classList.add("pop")
+    square2.classList.add("pop")
+    square1.style.border = "none"
+    square2.style.border = "none"
+square1.removeEventListener("click", squareClicked)
+square2.removeEventListener("click", squareClicked)
+    clickCount = 0;
+    console.log("is a match")
+}
+
+
+function checkGameEnded() {
+    const target = numberOfSquares / 2;
+    const gameOver = score === target ? true : false;
+    if(gameOver) {
+playAgainBtn.style.visibility= "visible"
+    }
+    function playAgain() {
+        window.location.reload(); 
+        
+    }
+}
+
+
+
+//selectColor();
+//selectColor();
+
+//console.log(selectColor());
+
